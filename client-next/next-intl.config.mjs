903964d16b6a63382544595951b248e77f894562
Jose Oldemar Chaves Urbina <oldemar.chaves@gmail.com>
2026-01-22 10:20:36 +0200

    Refactor: eliminar estructura antigua de Vite y carpeta temporal client-next

--- client-next/next-intl.config.mjs
+++
@@ -1,7 +0,0 @@
-export default {
-  // Estos son los idiomas que tu aplicación soportará
-  locales: ['en', 'es', 'fi', 'sv', 'so'],
-  
-  // El idioma por defecto si no se detecta ninguno o falla la ruta
-  defaultLocale: 'en'
-};
--- client-next/src/app/[locale]/(auth)/olivia/layout.jsx
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: 'Login | Admin Access',
-  robots: 'noindex, nofollow',
-};
-
-export default function LoginLayout({ children }) {
-  return <>{children}</>;
-}
