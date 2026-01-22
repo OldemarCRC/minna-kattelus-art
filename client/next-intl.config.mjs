78525dba354c9758615eb4eb16378be71636e804
Jose Oldemar Chaves Urbina <oldemar.chaves@gmail.com>
2026-01-22 10:26:31 +0200

    Chore: limpieza final de archivos legacy y configuracion de internacionalizacion

---
+++ client/next-intl.config.mjs
@@ -0,0 +1,7 @@
+export default {
+  // Estos son los idiomas que tu aplicación soportará
+  locales: ['en', 'es', 'fi', 'sv', 'so'],
+  
+  // El idioma por defecto si no se detecta ninguno o falla la ruta
+  defaultLocale: 'en'
+};
